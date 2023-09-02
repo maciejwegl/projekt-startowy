323edb72634300b5bfdbe2dce973d52cdbce08ab
maciejwegl <maciej.wegl@gmail.com>
2023-09-02 16:33:08 +0200

    calculatorUpdate

---
+++ script.js
@@ -0,0 +1,32 @@
+const currentNumber = document.querySelector(".currentNumber");
+
+const previousNumber = document.querySelector(".previousNumber");
+
+const mathSign = document.querySelector(".mathSign");
+
+const numberButtons = document.querySelectorAll(".number");
+
+const operatorButtons = document.querySelectorAll(".operator");
+
+const equalsButton = document.querySelector(".equals");
+
+const clearButton = document.querySelector(".clear");
+
+const calculatorHistory = document.querySelector(".history");
+
+const historyButton = document.querySelector(".history-btn");
+
+
+let result = "";
+
+
+
+
+
+
+
+
+
+
+
+// nasłuchiwanie przycisków //
